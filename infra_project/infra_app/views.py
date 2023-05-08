5162e6c0b336484656a9e27ad189f0b84a2f0f69
Ramil Khankildiev <ramil.khankildiev@gmail.com>
2023-05-09 00:44:33 +0200

    Modify welcome phrases

--- infra_project/infra_app/views.py
+++ infra_project/infra_app/views.py
@@ -2,8 +2,8 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse('У меня получилось!')
+    return HttpResponse('У меня получилось ВООБЩЕ 111!')
 
 
 def second_page(request):
-    return HttpResponse('А это вторая страница!')
+    return HttpResponse('А это вторая страница! INDEED!!')
